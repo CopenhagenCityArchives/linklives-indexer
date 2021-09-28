a167c0b6d6b413a0e403d1159ba86c9ebd33dc72
Lars Høgedal <lars.hogedal@gmail.com>
2021-09-28 10:53:30 +0200

    updated linklives lib

--- linklibes-indexer-test/UnitTest1.cs
+++ linklibes-indexer-test/UnitTest1.cs
@@ -1,6 +1,7 @@
 using Linklives.Domain;
 using Linklives.Indexer.Domain;
 using NUnit.Framework;
+using System.Linq;
 
 namespace linklibes_indexer_test
 {
@@ -15,8 +16,8 @@ namespace linklibes_indexer_test
         public void Sandbox()
         {
             var dataset = new DataSet<StandardPA>(@"F:\link-lives\LL_data_v1.0.dev0\development\standardized_sources\CBP\CBP.csv");
-            var pas = dataset.Read();
-            Assert.IsNotNull(pas);
+            var pas = dataset.Read().Select(std => BasePA.Create(10, std)).ToList();
+            Assert.IsNotNull(pas[0]);
         }
     }
 }
