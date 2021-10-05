915a47f49c66602332984b02aa23807e6b3bea7f
Lars Høgedal <lars.hogedal@gmail.com>
2021-10-05 14:08:50 +0200

    Implemented basic ES indexing

---
+++ linklives-indexer-Lib/Utils/ESHelper.cs
@@ -0,0 +1,60 @@
+﻿using log4net;
+using Nest;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Linklives.Indexer.Utils
+{
+    public class ESHelper
+    {
+        private readonly Nest.ElasticClient _esClient;
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+
+        public ESHelper(ElasticClient esClient)
+        {
+            _esClient = esClient;
+        }
+        public string CreateNewIndex<T>(string index) where T : class
+        {
+            var indexname = $"{index}_{DateTime.Now.ToString("dd-MM-yyyy_hh-mm-ss")}";
+            _esClient.Indices.Create(indexname, c => c.
+                Map<T>(m => m.AutoMap()));
+            return indexname;
+        }
+        public void ActivateNewIndices(IDictionary<string, string> indexMappings)
+        {
+            foreach (var indexMap in indexMappings)
+            {
+                ActivateNewIndex(indexMap.Key, indexMap.Value);
+            }
+        }
+        public void ActivateNewIndex(string index, string indexName)
+        {
+            //Cleanup existing aliases
+            _esClient.Indices.DeleteAlias($"{index}*", "_all");
+            //Add our new alias
+            _esClient.Indices.PutAlias(index, indexName);
+            //Find and delete all the old versions of this index
+            var oldIndices = _esClient.Indices.Get($"{index}*").Indices.Where(i => i.Key.Name != indexName);
+            foreach (var oldIndex in oldIndices)
+            {
+                _esClient.Indices.Delete(oldIndex.Key.Name);
+            }
+        }
+        public void BulkIndexDocs<T>(IEnumerable<T> docs, string index) where T : class
+        {
+            //TODO: Add fetch some of these config values from enviroment config instead of hardcording.
+            var bulkAllObservable = _esClient.BulkAll(docs, b => b
+               .Index(index)
+               .BackOffTime("30s")
+               .BackOffRetries(3)
+               .RefreshOnCompleted()
+               .MaxDegreeOfParallelism(Environment.ProcessorCount)
+               .Size(5000))
+               .Wait(TimeSpan.FromHours(3), onNext: response => { Log.Info($"Page: {response.Page} containing: {response.Items.Count} items sucessfully indexed to {index}"); });
+        }
+    }
+}
--- linklives-lifecourse-indexer/Program.cs
+++ linklives-lifecourse-indexer/Program.cs
@@ -1,29 +1,82 @@
 ﻿using Linklives.Domain;
 using Linklives.Indexer.Domain;
-using System;
+using Linklives.Indexer.Utils;
+using log4net;
+using log4net.Config;
 using Nest;
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+using System.Reflection;
 
 namespace Linklives.Indexer.Lifecourses
 {
     class Program
     {
-        static void Main(string[] args)
+        private static ILog Log;
+        static int Main(string[] args)
         {
-            var esClient = new ElasticClient(new ConnectionSettings(new Uri("ElasticSearch-URL"))
-                .RequestTimeout(TimeSpan.FromMinutes(2))
-                .DisableDirectStreaming());
+            Initconfig();
+            var cmd = new RootCommand
+            {
+                new Option<string>("--path", "The path to the datasets top level folder"),
+                new Option<string>("--es-host", "The url of the elastic search server to use for this indexation"),
+                new Option<string>("--api-host", "The url of the linklives api server to use for this indexation"),
+                new Option<int>("--lifecourses-count", getDefaultValue: ()=> 0, "the maximum ammount of lifecourses to index, 0 indicates that all lifecourses should be indexed."),
+            };
+
+            cmd.Handler = CommandHandler.Create<string, string, string, int>(Index);
+
+            return cmd.Invoke(args);
         }
-        private IEnumerable<BasePA> ReadPAs(string basePath)
+        private static void Initconfig()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+            Log = LogManager.GetLogger(System.Reflection.MethodInfo.GetCurrentMethod().DeclaringType.Name);
+        }
+        static void Index(string path, string esHost, string apiHost, int maxLifecourses)
+        {
+            var esClient = new ElasticClient(new ConnectionSettings(new Uri(esHost))
+               .RequestTimeout(TimeSpan.FromMinutes(2))
+               .DisableDirectStreaming());
+            var indexHelper = new ESHelper(esClient);
+            var AliasIndexMapping = SetUpNewIndexes(indexHelper);
+
+            indexHelper.BulkIndexDocs(ReadPAs(path), AliasIndexMapping["pas"]);
+            //Do all the indexing stuff
+            indexHelper.ActivateNewIndices(AliasIndexMapping);
+        }
+        private static IDictionary<string,string> SetUpNewIndexes(ESHelper indexHelper)
+        {
+            var result = new Dictionary<string, string>();
+            result["pas"] = indexHelper.CreateNewIndex<BasePA>("pas");
+            result["links"] = indexHelper.CreateNewIndex<Link>("links");
+            result["lifecourses"] = indexHelper.CreateNewIndex<LifeCourse>("lifecourses");
+            result["sources"] = indexHelper.CreateNewIndex<Source>("sources");
+            return result;
+        }
+        private static IEnumerable<BasePA> ReadPAs(string basePath)
         {
             var sources = new DataSet<Source>($"{basePath}\\auxilary_data\\sources\\sources.csv");
             foreach (var source in sources.Read())
             {
                 var paSet = new DataSet<StandardPA>($"{basePath}\\{source.File_reference}");
-                foreach (var pa in paSet.Read())
+                foreach (var stdPa in paSet.Read())
                 {
-                    yield return BasePA.Create(source.Source_id, pa);
+                    BasePA pa = null;
+                    try
+                    {
+                        pa = BasePA.Create(source.Source_id, stdPa);
+                    }
+                    catch (Exception)
+                    {
+
+                        throw;
+                    }
+                    yield return pa;
                 }
             }
         }
