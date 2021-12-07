e152b410ba01927e08567ea81e753a3b0a4d6b8e
Bo Henriksen <stumpdenmark@hotmail.com>
2021-12-07 20:43:09 +0100

    Make it possible to skip database upserts and/or pas indexation

--- linklives-lifecourse-indexer/Program.cs
+++ linklives-lifecourse-indexer/Program.cs
@@ -33,10 +33,12 @@ namespace Linklives.Indexer.Lifecourses
                 new Option<string>("--trs-path", "The path to the datasets top level folder"),
                 new Option<string>("--es-host", "The url of the elastic search server to use for this indexation"),
                 new Option<string>("--db-conn", "The url of the linklives api server to use for this indexation"),
+                new Option<bool>("--skip-db", getDefaultValue: ()=> false,"Indicates if the database upserts should be skipped"),
+                new Option<bool>("--skip-pas", getDefaultValue: ()=> false,"Indicates if the pas indexation should be skipped"),
                 new Option<int>("--max-entries", getDefaultValue: ()=> 0, "the maximum ammount of entries to index, 0 indicates that all entries should be indexed."),
             };
 
-            cmd.Handler = CommandHandler.Create<string, string, string, string, int>(Index);
+            cmd.Handler = CommandHandler.Create<string, string, string, string, bool, bool, int>(Index);
 
             return cmd.Invoke(args);
         }
@@ -45,7 +47,7 @@ namespace Linklives.Indexer.Lifecourses
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
         }
-        static void Index(string llPath, string trsPath, string esHost, string dbConn, int maxEntries)
+        static void Index(string llPath, string trsPath, string esHost, string dbConn, bool skipDb, bool skipPas, int maxEntries)
         {
             #region ES Setup
             var esClient = new ElasticClient(new ConnectionSettings(new Uri(esHost))
@@ -68,6 +70,7 @@ namespace Linklives.Indexer.Lifecourses
             #endregion
             
             var AliasIndexMapping = SetUpNewIndexes(indexHelper);
+            
             var indextimer = Stopwatch.StartNew();
             var datasetTimer = Stopwatch.StartNew();
             Log.Info("Reading lifecourses");
@@ -75,21 +78,31 @@ namespace Linklives.Indexer.Lifecourses
             Log.Info("Indexing lifecourses");
             indexHelper.BulkIndexDocs(lifecourses, AliasIndexMapping["lifecourses"]);
             Log.Info($"Finished indexing lifecourses. took {datasetTimer.Elapsed}");
+            
             datasetTimer.Restart();
             Log.Info("Discarding duplicate lifecourses");
             var beforecount = lifecourses.Count();
             lifecourses = lifecourses.GroupBy(x => x.Key).Select(x => x.First()).ToList();
             Log.Info($"Discarded {beforecount - lifecourses.Count()} lifecourses while checking for duplicate keys. Took {datasetTimer.Elapsed}");
-            var lifecourseRepo = new EFLifeCourseRepository(dbContext);
 
             // Data version as given in WP3 data
             //TODO Set somewhere else
             DataVersion = "1.0";
             if (DataVersion == null) { throw new Exception("DataVersion is not set. Cannot continue"); }
+            
             datasetTimer.Restart();
-            Log.Info($"Upserting {lifecourses.Count()} lifecourses, marking old ones");
-            lifecourseRepo.InsertItemsUpdateExistingItems(lifecourses, DataVersion);
-            Log.Info($"Done upserting. Took {datasetTimer.Elapsed}");
+
+            if (skipDb)
+            {
+                Log.Info("Skipping database upserts");
+            }
+            else
+            {
+                Log.Info($"Upserting {lifecourses.Count()} lifecourses, marking old ones");
+                var lifecourseRepo = new EFLifeCourseRepository(dbContext);
+                lifecourseRepo.InsertItemsUpdateExistingItems(lifecourses, DataVersion);
+                Log.Info($"Done upserting. Took {datasetTimer.Elapsed}");
+            }
 
             var pasInLifeCourses = new Dictionary<string, List<int>>();
 
@@ -113,51 +126,62 @@ namespace Linklives.Indexer.Lifecourses
             lifecourses.Clear(); //free up some memory space
             Log.Info($"Finished building pasInLifecourses dictionary. Took {datasetTimer.Elapsed}");
             datasetTimer.Restart();
-      
+
+            
             try
             { 
-                Log.Info("Indexing person appearances");
                 var sources = new DataSet<Source>(Path.Combine(llPath, "auxilary_data", "sources", "sources.csv")).Read().ToList();
-                Parallel.ForEach(sources, new ParallelOptions { MaxDegreeOfParallelism = 1 }, source =>
+                
+                if (skipPas)
                 {
-                    Log.Debug($"Reading PAs from source {source.Source_name}");
-                    var timer = Stopwatch.StartNew();
-                    var sourcePAs = ReadSourcePAs(llPath, source, trsPath, pasInLifeCourses);
-                    Log.Debug($"Indexing PAs from source {source.Source_name}");
-                    //indexHelper.BulkIndexDocs(sourcePAs, AliasIndexMapping["pas"]);
-                    var paBatch = new List<BasePA>();
-                    foreach(var curPa in sourcePAs)
+                    Log.Info("Skipping indexation of person apperances");
+                }
+                else
+                {
+                    Log.Info("Indexing person appearances");
+
+                    Parallel.ForEach(sources, new ParallelOptions { MaxDegreeOfParallelism = 1 }, source =>
                     {
-                        paBatch.Add(curPa);
-                        if(paBatch.Count == 3000)
+                        Log.Debug($"Reading PAs from source {source.Source_name}");
+                        var timer = Stopwatch.StartNew();
+                        var sourcePAs = ReadSourcePAs(llPath, source, trsPath, pasInLifeCourses);
+                        Log.Debug($"Indexing PAs from source {source.Source_name}");
+                        //indexHelper.BulkIndexDocs(sourcePAs, AliasIndexMapping["pas"]);
+                        var paBatch = new List<BasePA>();
+                        foreach (var curPa in sourcePAs)
                         {
-                            try { 
-                                indexHelper.IndexManyDocs(paBatch, AliasIndexMapping["pas"]);
-                            }
-                            catch(Exception e)
+                            paBatch.Add(curPa);
+                            if (paBatch.Count == 3000)
                             {
-                                Console.WriteLine(e.Message);
+                                try
+                                {
+                                    indexHelper.IndexManyDocs(paBatch, AliasIndexMapping["pas"]);
+                                }
+                                catch (Exception e)
+                                {
+                                    Console.WriteLine(e.Message);
+                                }
+                                UpdateLifecourses(esClient, paBatch, pasInLifeCourses, AliasIndexMapping["lifecourses"]);
+
+                                paBatch.Clear();
                             }
+                        }
+
+                        // End of iteration, flush
+                        if (paBatch.Count > 0)
+                        {
+                            indexHelper.IndexManyDocs(paBatch, AliasIndexMapping["pas"]);
                             UpdateLifecourses(esClient, paBatch, pasInLifeCourses, AliasIndexMapping["lifecourses"]);
 
                             paBatch.Clear();
                         }
-                    }
 
-                    // End of iteration, flush
-                    if(paBatch.Count > 0)
-                    {
-                        indexHelper.IndexManyDocs(paBatch, AliasIndexMapping["pas"]);
-                        UpdateLifecourses(esClient, paBatch, pasInLifeCourses, AliasIndexMapping["lifecourses"]);
+                        Log.Debug($"Finished indexing PAs from source {source.Source_name}. Took: {timer.Elapsed}");
+                    });
 
-                        paBatch.Clear();
-                    }
-
-                    Log.Debug($"Finished indexing PAs from source {source.Source_name}. Took: {timer.Elapsed}");
-                });
-                
-                Log.Info($"Finished indexing person appearances. Took {datasetTimer.Elapsed}");
-                datasetTimer.Restart();
+                    Log.Info($"Finished indexing person appearances. Took {datasetTimer.Elapsed}");
+                    datasetTimer.Restart();
+                }
 
                 Log.Info("Indexing sources");
                 indexHelper.BulkIndexDocs(sources, AliasIndexMapping["sources"]);
@@ -171,7 +195,7 @@ namespace Linklives.Indexer.Lifecourses
             }
             catch(Exception e)
             {
-               Log.Warn("Could not complete indexation: " + e.Message);
+                Log.Warn("Could not complete indexation: " + e.Message);
                 Log.Info("Removing new indexes");
                 foreach(var mapping in AliasIndexMapping){
                     Log.Info($"Removing index {mapping.Value}");
