83a5ea87ab32e8a1f9ebdb89e95a42cab4c3f4f7
Bo Henriksen <stumpdenmark@hotmail.com>
2023-01-06 16:39:13 +0100

    Use bash to execute script

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash 
 
-# tag and push linklives-transcribed-indexer image
+#tag and push linklives-transcribed-indexer image
 docker tag linklives-transcribed-indexer 282251075226.dkr.ecr.eu-west-1.amazonaws.com/linklives-transcribed-indexer:{image-tag}
 docker push 282251075226.dkr.ecr.eu-west-1.amazonaws.com/linklives-transcribed-indexer:{image-tag}
 
-# tag and push linklives-lifecourse-indexer image
+#tag and push linklives-lifecourse-indexer image
 docker tag linklives-lifecourse-indexer 282251075226.dkr.ecr.eu-west-1.amazonaws.com/linklives-lifecourse-indexer:{image-tag}
 docker push 282251075226.dkr.ecr.eu-west-1.amazonaws.com/linklives-lifecourse-indexer:{image-tag}
